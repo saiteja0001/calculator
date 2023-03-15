26febfb16e2b5f9e67bec56d117b2f917df9f9b2
Sai Teja Mogatadakala <saiteja@accionlabs.com>
2023-03-15 14:52:59 +0530

    fix undefined err

--- calc.js
+++ calc.js
@@ -1,16 +1,17 @@
-function displaydata(data){
-    result.value+=data
- }
- function allclear(){
-     result.value=" "
- }
- 
- function equal(){
-    console.log(result)
-    result.value= eval(result.value)
- }
- 
- function backspace(){
-     data=result.value
-     result.value=data.slice(0,-1)
- }
+function displaydata(data) {
+  result.value += data;
+}
+function allclear() {
+  result.value = "";
+}
+
+function equal() {
+  if (result.value) {
+    result.value = eval(result.value);
+  }
+}
+
+function backspace() {
+  data = result.value;
+  result.value = data.slice(0, -1);
+}
